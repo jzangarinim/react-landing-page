1f141666f7e8b2d9bb02fded3cab6a289b51b7fc
Jose Zangarini <jzangarinim@gmail.com>
2023-05-08 17:54:55 -0400

    added work in progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">React Landing Page</h1>
+      <a href="#" className="btn btn-success">
+        Work In Progress...
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
